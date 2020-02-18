1f306d3510f66322c13bf8d56c723d43105edb2a
George Nizaradze <george.nizharadze@sky.com>
2020-02-18 11:21:32 +0100

    fix: Correct linting issues

--- plumber_ml.r
+++ plumber_ml.r
@@ -10,9 +10,9 @@ function() {
 
 #' Predict house price
 #' @param crim:numeric per capita crime rate by town
-#' @param zn:numeric proportion of residential land zoned for lots over 25,000 sq.ft.
+#' @param zn:numeric proportion of resid land zoned for lots over 25k sq.ft.
 #' @param indus:numeric proportion of non-retail business acres per town
-#' @param chas:int Charles River dummy variable, 1 if tract bounds river; 0 otherwise
+#' @param chas:int dummy var, 1 if tract bounds Charles river; 0 otherwise
 #' @param nox:numeric nitric oxides concentration, parts per 10 million
 #' @param rm:numeric average number of rooms per dwelling
 #' @param age:numeric proportion of owner-occupied units built prior to 1940
@@ -20,7 +20,7 @@ function() {
 #' @param rad:numeric index of accessibility to radial highways
 #' @param tax:numeric full-value property-tax rate per $10,000
 #' @param ptratio:numeric pupil-teacher ratio by town
-#' @param b:numeric 1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
+#' @param b:numeric 1000(Bk - 0.63)^2; Bk is proportion of blacks by town
 #' @param lstat:numeric % lower status of the population
 #' @get /predict_price
 function(crim, zn, indus, chas, nox, rm, age, dis, rad, tax, ptratio, b, lstat) {
